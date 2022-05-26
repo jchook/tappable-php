c3cbacccdbb0c0364f334be7fdc5517ccc719a05
Wes Roberts <takq@a.zinc.email>
2022-05-26 19:53:06 -0400

    smtp: stop-gap with converting NewSession to the Agent namespace

--- src/Smtp/Role/Server/Action/NewSession.php
+++ src/Smtp/Role/Server/Action/NewSession.php
@@ -2,9 +2,10 @@
 
 namespace Tap\Smtp\Role\Server\Action;
 
+use Tap\Smtp\Role\Agent\Action\NewSession as AgentNewSession;
 use Tap\Smtp\Session\Session;
 
-class NewSession extends ServerAction
+class NewSession extends AgentNewSession
 {
   public function __construct(
     public Session $session,
